0cd0cfcdb76872cd9857bfaf4eec699cddc05d8e
zongf0504 <15737250504@139.com>
2019-12-15 15:13:05 +0800

    移除freemarker 模块儿

--- auto-generator/src/main/java/org/zongf/auto/generator/utils/TemplateUtil.java
+++ auto-generator/src/main/java/org/zongf/auto/generator/utils/TemplateUtil.java
@@ -1,7 +1,9 @@
-package org.zongf.db.meta.mysql.utils;
+package org.zongf.auto.generator.utils;
 
 import freemarker.template.Configuration;
 import freemarker.template.Template;
+import org.zongf.db.meta.mysql.utils.IOUtils;
+
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.OutputStreamWriter;
@@ -17,13 +19,15 @@ public class TemplateUtil {
     private static Configuration cfgConfig;
 
     // ftl模板文件根目录
-    private static final String FTL_ROOT_PATH ="src/main/resources/ftl";
+    private static final String FTL_ROOT_PATH ="/ftl";
 
     static {
         try {
             // 模板存放目录
             cfgConfig = new Configuration(Configuration.VERSION_2_3_22);
-            cfgConfig.setDirectoryForTemplateLoading(new File(FTL_ROOT_PATH));
+//            cfgConfig.setDirectoryForTemplateLoading(new File(FTL_ROOT_PATH));
+            // 封装为jar包后,需要使用classLoader 方式加载jar包中的目标文件
+            cfgConfig.setClassLoaderForTemplateLoading(TemplateUtil.class.getClassLoader(),FTL_ROOT_PATH);
             cfgConfig.setDefaultEncoding("UTF-8");  //设置编码为UTF-8
             cfgConfig.setNumberFormat("####");        //设置数字格式，默认三位数字用,号分割
         } catch (Exception ex) {
