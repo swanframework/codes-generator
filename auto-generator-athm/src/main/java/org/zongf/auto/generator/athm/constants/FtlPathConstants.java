5513c8d3a5989252461162ba15c86a117c6d5aa8
zonggaofeng <zonggaofeng@autohome.com.cn>
2019-12-25 10:41:53 +0800

    调整包结构

--- auto-generator-athm/src/main/java/org/zongf/auto/generator/athm/constants/FtlPathConstants.java
+++ auto-generator-athm/src/main/java/org/zongf/auto/generator/athm/constants/FtlPathConstants.java
@@ -7,26 +7,26 @@ package org.zongf.auto.generator.athm.constants;
 public class FtlPathConstants {
 
     /** po 模板文件  */
-    public static final String FTL_BASE_PO = "base-entity.ftl";
+    public static final String FTL_BASE_PO = "base/base-entity.ftl";
 
     /** mapper 接口  */
-    public static final String FTL_BASE_MAPPER_API = "base-mapperApi.ftl";
+    public static final String FTL_BASE_MAPPER_API = "base/base-mapperApi.ftl";
 
     /** mapper 文件  */
-    public static final String FTL_BASE_MAPPER_IMPL = "base-mapperXml.ftl";
+    public static final String FTL_BASE_MAPPER_IMPL = "base/base-mapperXml.ftl";
 
     /** service 接口模板  */
-    public static final String FTL_BASE_SERVICE_API = "base-serviceApi.ftl";
+    public static final String FTL_BASE_SERVICE_API = "base/base-serviceApi.ftl";
 
     /** service 实现类  */
-    public static final String FTL_BASE_SERVICE_IMPL = "base-serviceImpl.ftl";
+    public static final String FTL_BASE_SERVICE_IMPL = "base/base-serviceImpl.ftl";
 
     /** controller  */
-    public static final String FTL_BASE_CONTROLLER = "base-controller.ftl";
+    public static final String FTL_BASE_CONTROLLER = "base/base-controller.ftl";
 
     /** dto 模板  */
-    public static final String FTL_MODEL_DTO = "model-dto.ftl";
+    public static final String FTL_MODEL_DTO = "model/model-dto.ftl";
 
     /** query 模板  */
-    public static final String FTL_MODEL_QUERY = "model-query.ftl";
+    public static final String FTL_MODEL_QUERY = "model/model-query.ftl";
 }
