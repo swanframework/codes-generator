fa2c05adcd5d209ddbb250a435a4454e86b9f638
zonggaofeng <zonggaofeng@autohome.com.cn>
2019-12-25 14:54:03 +0800

    初步完成重构

--- auto-generator-athm/src/main/java/org/zongf/auto/generator/athm/constants/FtlPathConstants.java
+++ auto-generator-athm/src/main/java/org/zongf/auto/generator/athm/constants/FtlPathConstants.java
@@ -7,26 +7,58 @@ package org.zongf.auto.generator.athm.constants;
 public class FtlPathConstants {
 
     /** po 模板文件  */
-    public static final String FTL_BASE_PO = "base/base-entity.ftl";
+    public static final String FTL_BASE_PO = "base/entity.ftl";
 
     /** mapper 接口  */
-    public static final String FTL_BASE_MAPPER_API = "base/base-mapperApi.ftl";
+    public static final String FTL_BASE_MAPPER_API = "base/mapperApi.ftl";
 
     /** mapper 文件  */
-    public static final String FTL_BASE_MAPPER_IMPL = "base/base-mapperXml.ftl";
+    public static final String FTL_BASE_MAPPER_IMPL = "base/mapperXml.ftl";
 
     /** service 接口模板  */
-    public static final String FTL_BASE_SERVICE_API = "base/base-serviceApi.ftl";
+    public static final String FTL_BASE_SERVICE_API = "base/serviceApi.ftl";
 
     /** service 实现类  */
-    public static final String FTL_BASE_SERVICE_IMPL = "base/base-serviceImpl.ftl";
+    public static final String FTL_BASE_SERVICE_IMPL = "base/serviceImpl.ftl";
 
     /** controller  */
-    public static final String FTL_BASE_CONTROLLER = "base/base-controller.ftl";
+    public static final String FTL_BASE_CONTROLLER = "base/controller.ftl";
 
     /** dto 模板  */
-    public static final String FTL_MODEL_DTO = "model/model-dto.ftl";
+    public static final String FTL_MODEL_DTO = "model/dto.ftl";
 
     /** query 模板  */
-    public static final String FTL_MODEL_QUERY = "model/model-query.ftl";
+    public static final String FTL_MODEL_QUERY = "model/query.ftl";
+
+    /** open 模块儿: remote service API */
+    public static final String FTL_OPEN_REMOTE_SERVICE_API = "open/remote-service-api.ftl";
+
+    /** open 模块儿: remote service 实现 */
+    public static final String FTL_OPEN_REMOTE_SERVICE_IMPL = "open/remote-service-impl.ftl";
+
+    /** open 模块儿: service API */
+    public static final String FTL_OPEN_SERVICE_API = "open/service-api.ftl";
+
+    /** open 模块儿: service 实现 */
+    public static final String FTL_OPEN_SERVICE_IMPL = "open/service-impl.ftl";
+
+    /** open 模块儿: service 实现 */
+    public static final String FTL_OPEN_IMPL = "open/controller.ftl";
+
+    /** manager 模块儿: remote service API */
+    public static final String FTL_MANAGER_REMOTE_SERVICE_API = "manager/remote-service-api.ftl";
+
+    /** manager 模块儿: remote service 实现 */
+    public static final String FTL_MANAGER_REMOTE_SERVICE_IMPL = "manager/remote-service-impl.ftl";
+
+    /** manager 模块儿: service API */
+    public static final String FTL_MANAGER_SERVICE_API = "manager/service-api.ftl";
+
+    /** manager 模块儿: service 实现 */
+    public static final String FTL_MANAGER_SERVICE_IMPL = "manager/service-impl.ftl";
+
+    /** manager 模块儿: service 实现 */
+    public static final String FTL_CONTROLLER_IMPL = "manager/controller.ftl";
+
+
 }
