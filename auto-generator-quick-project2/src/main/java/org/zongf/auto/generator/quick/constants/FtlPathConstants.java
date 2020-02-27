76fd27dafff3e4c89640cea004c6702627a1f9cd
zonggaofeng <zonggaofeng@autohome.com.cn>
2020-02-27 10:00:35 +0800

    调整包结构

---
+++ auto-generator-quick-project2/src/main/java/org/zongf/auto/generator/quick/constants/FtlPathConstants.java
@@ -0,0 +1,30 @@
+package org.zongf.auto.generator.quick.constants;
+
+/** 模板文件路径
+ * @author zongf
+ * @date 2019-11-30
+ */
+public class FtlPathConstants {
+
+    /** entity 模板文件  */
+    public static final String FTL_ENTITY = "entity.ftl";
+
+    /** fieldEnum 模板文件  */
+    public static final String FTL_FIELD_ENUM = "fieldEnum.ftl";
+
+    /** mapper 接口模板文件  */
+    public static final String FTL_MAPPER_API = "mapperApi.ftl";
+
+    /** mapper 映射模板文件  */
+    public static final String FTL_MAPPER_XML = "mapperXml.ftl";
+
+    /** service 接口类模板文件  */
+    public static final String FTL_SERVICE_API = "serviceApi.ftl";
+
+    /** service 实现类模板文件  */
+    public static final String FTL_SERVICE_IMPL = "serviceImpl.ftl";
+
+    /** controller 模板文件  */
+    public static final String FTL_CONTROLLER = "controller.ftl";
+
+}
